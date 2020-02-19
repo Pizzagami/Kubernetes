f0fec40ce78b41a6542893197d5e2ab1b36d7cd7
Samuel ELGRABLY <selgrabl@e2r3p6.42.fr>
2020-02-19 17:17:45 +0100

    coucou

--- srcs/nginx/srcs/data_base.sql
+++
@@ -1,5 +0,0 @@
-CREATE DATABASE wordpress CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-GRANT ALL PRIVILEGES ON wordpress.* TO username @'localhost' IDENTIFIED BY 'password';
-
-CREATE USER 'selgrabl' @'localhost' IDENTIFIED BY 'password';
-GRANT ALL PRIVILEGES ON *.* TO  'selgrabl' @'localhost' WITH GRANT OPTION;
