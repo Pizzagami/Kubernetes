8350e330ff02cc1b2d23aa65f1779ae6750928d8
Samuel ELGRABLY <selgrabl@e2r9p16.42.fr>
2020-03-03 18:28:55 +0100

    bordel

--- setup.sh
+++ setup.sh
@@ -1,3 +1,10 @@
 rm -rf ~/.ssh/known_hosts
+
+MINIKUBE_IP=$(minikube ip)
+minikube addons enable metrics-server
+minikube addons enable ingress
+eval $(minikube docker-env)
+
+docker build -t nginx:1.11 srcs/nginx
 docker-compose -f ./srcs/mysql/docker-compose.yaml up -d
-docker-compose -f ./srcs/nginx/docker-compose.yaml up -d
+#docker-compose -f ./srcs/nginx/docker-compose.yaml up -d
