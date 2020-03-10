ba9c6fee3ad481049f120dd29770a6b4b94b2bcb
Samuel ELGRABLY <selgrabl@e2r7p1.42.fr>
2020-03-10 19:28:57 +0100

    wordpress finish the linking

--- setup.sh
+++ setup.sh
@@ -15,7 +15,7 @@ docker build -t nginx_ssh srcs/nginx
 #docker build -t ftps_server srcs/ftps
 #docker build -t wordpr srcs/wordpress
 #docker build -t phpadm srcs/phpmyadmin
-#docker build -t maria srcs/mysql
+docker build -t maria srcs/mysql
 
 kubectl apply -k srcs
 minikube dashboard
