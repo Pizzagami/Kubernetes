508dfc797f7a709312d292fd62613fcf45922825
Velovo <velov.vanpraet@gmail.com>
2020-08-30 17:05:26 +0200

    yoooooooooo

--- srcs/mysql/init.sql
+++ srcs/mysql/init.sql
@@ -3,6 +3,9 @@ CREATE DATABASE wordpress;
 CREATE USER 'admin'@'%' IDENTIFIED BY 'admin';
 GRANT ALL ON *.* TO 'admin'@'%' IDENTIFIED BY 'admin' WITH GRANT OPTION;
 
+CREATE USER 'wordpressuser'@'%' IDENTIFIED BY 'pswd';
+GRANT ALL PRIVILEGES ON wordpress.* TO  'wordpressuser' @'%' WITH GRANT OPTION;
+
 CREATE USER 'selgrabl'@'%' IDENTIFIED BY 'pswd';
 GRANT ALL PRIVILEGES ON wordpress.* TO  'selgrabl' @'%' WITH GRANT OPTION;
 
--- srcs/wordpress/entrypoint.sh
+++
@@ -1,3 +0,0 @@
-wget -c https://wordpress.org/latest.tar.gz -O - | tar -xz -C /var/www
-chown -R www-data: /var/www/wordpress/
-php -S 0.0.0.0:5050 -t /www/wordpress/
