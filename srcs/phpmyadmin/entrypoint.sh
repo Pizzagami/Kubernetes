70d7af1132906a66d0ce917d22cdc1d1f45b2baf
Velovo <velov.vanpraet@gmail.com>
2020-08-30 15:40:21 +0200

    oups

--- srcs/phpmyadmin/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-rc-service php-fpm7 start
-wget -c https://files.phpmyadmin.net/phpMyAdmin/4.9.2/phpMyAdmin-4.9.2-english.tar.gz -O - | tar -xz -C /var/www
-mv var/www/phpMyAdmin-4.9.2-english var/www/phpmyadmin
-chown -R www-data: /var/www/phpmyadmin
-tail -f /dev/null
-
-service nginx restart
---
+++ srcs/phpmyadmin/phpmyadm.sh
@@ -0,0 +1,1 @@
+php -S 0.0.0.0:5000 -t /var/www/phpMyAdmin-5.0.2-all-languages
