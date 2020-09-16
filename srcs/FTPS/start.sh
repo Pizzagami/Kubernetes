61a02d6f9b1f85a3260ef14e53b217b86b0cf2bf
pizzagami <selgrabl@student.42.fr>
2020-09-16 22:31:02 +0200

    ftps t1

--- srcs/FTPS/start.sh
+++ srcs/FTPS/start.sh
@@ -1,2 +1,4 @@
-#fouttre l'ip depuis le fichier
-pure-ftpd -p 21000:21000 # -P $(cat ip.txt)
+mkdir -p ftp/user
+chown user:user ftp/user
+
+exec /usr/sbin/vsftpd -opasv_min_port=21000 -opasv_max_port=21000 -opasv_address=192.168.99.127 /etc/vsftpd/vsftpd.conf
