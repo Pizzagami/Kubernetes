de26ae9ea2590ee03b83bd084316a9a262421e51
selgrabl <selgrabl@42.student.fr>
2020-09-17 02:22:40 +0200

    ftps done

--- srcs/FTPS/start.sh
+++ srcs/FTPS/start.sh
@@ -5,4 +5,4 @@ echo -e "password : password" | adduser -h ftp/user -s /sbin/nologin user
 mkdir -p ftp/user
 chown user:user ftp/user
 
-exec /usr/sbin/vsftpd -opasv_min_port=21000 -opasv_max_port=21000 -opasv_address=192.168.99.127 /etc/vsftpd/vsftpd.conf
+exec /usr/sbin/vsftpd -opasv_min_port=21000 -opasv_max_port=21000 -opasv_address=172.17.0.5 /etc/vsftpd/vsftpd.conf
